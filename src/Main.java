ea533e8a32a76563948cb020ce1d7c90c7dfb62c
Argentumkuh <Argentumkuh@gmail.com>
2018-03-02 14:24:09 +0200

    HelloWorld with compilation error.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!")
+        Integer data = "hello";
+        System.out.println("Hello World!");
     }
 }
